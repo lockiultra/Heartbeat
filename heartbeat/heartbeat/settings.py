606802ecc6b341a93da57ff78def8c8621ce80c5
lockiultra <lockiultra@gmail.com>
2023-04-01 05:36:37 +0300

    Add menu app to INSTALLED_APPS

--- heartbeat/heartbeat/settings.py
+++ heartbeat/heartbeat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^an@k=geicx57u(f-m&$n&xpd48vp^d5n13uks$t89*of98dpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'menu.apps.MenuConfig'
 ]
 
 MIDDLEWARE = [
