6b539b04e2e0fb7ca2b0652a617a77d0b62907ec
lockiultra <lockiultra@gmail.com>
2023-04-01 05:37:24 +0300

    Add MenuView

--- heartbeat/menu/views.py
+++ heartbeat/menu/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from menu.models import MenuItem
 
 # Create your views here.
+
+class MenuView(ListView):
+    model = MenuItem
