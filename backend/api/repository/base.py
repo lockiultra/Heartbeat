10b452bd353266afe97c4ab2c9eaae580715d742
lockiultra <lockiultra@gmail.com>
2025-02-07 04:09:23 +0300

    Some typing fixes

--- backend/api/repository/base.py
+++ backend/api/repository/base.py
@@ -1,3 +1,4 @@
+from typing import Type, Sequence
 from backend.core.db import Base
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -9,12 +10,12 @@ from fastapi import HTTPException, status
 
 class BaseRepository:
     @classmethod
-    async def get_all(cls, db: AsyncSession, orm_model: Base) -> list[User]:
+    async def get_all(cls, db: AsyncSession, orm_model: Type[Base]) -> Sequence[Base]:
         result = await db.execute(select(orm_model))
         return result.scalars().all()
 
     @classmethod
-    async def get_by_id(cls, db: AsyncSession, id: int, orm_model: Base) -> User:
+    async def get_by_id(cls, db: AsyncSession, id: int, orm_model: Type[Base]) -> User:
         result = await db.execute(select(orm_model).where(orm_model.id == id))
         result = result.scalars().first()
         if result is None:
@@ -22,7 +23,7 @@ class BaseRepository:
         return result
 
     @classmethod
-    async def create(cls, db: AsyncSession, orm_model: Base, create_scheme: UserCreate)  -> User:
+    async def create(cls, db: AsyncSession, orm_model: Type[Base], create_scheme: UserCreate)  -> User:
         data = create_scheme.model_dump()
         new_model = orm_model(**data)
         new_model.password = get_password_hash(new_model.password)
@@ -32,7 +33,7 @@ class BaseRepository:
         return new_model
 
     @classmethod
-    async def update(cls, db: AsyncSession, orm_model: Base, update_scheme: UserUpdate) -> User:
+    async def update(cls, db: AsyncSession, orm_model: Type[Base], update_scheme: UserUpdate) -> User:
         data = update_scheme.model_dump()
         model = await cls.get_by_id(db, update_scheme.id, orm_model)
         for key, value in data.items():
@@ -43,7 +44,7 @@ class BaseRepository:
         return model
 
     @classmethod
-    async def delete(cls, db: AsyncSession, orm_model: Base, delete_scheme: UserDelete) -> User:
+    async def delete(cls, db: AsyncSession, orm_model: Type[Base], delete_scheme: UserDelete) -> User:
         model = await cls.get_by_id(db, delete_scheme.id, orm_model)
         await db.delete(model)
         await db.commit()
